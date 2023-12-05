6b5f275e6ada460a9dd88a3955f989db142d7def
Halid <halid.gundz@gmail.com>
2023-12-05 13:28:12 +0100

    Buyazon Created

---
+++ src/main/java/com/eshop/buyazon/BuyazonApplication.java
@@ -0,0 +1,13 @@
+package com.eshop.buyazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyazonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyazonApplication.class, args);
+	}
+
+}
